a65c8ca886f1afd36ca6dbb75a4908a51d94d7ce
Slavchu <tothelove1@gmail.com>
2023-09-15 19:28:10 +0300

    Adding database, fixing cmake

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <curl/curl.h>
 #include <thread_manager.hpp>
 #include <bot_api.hpp>
+#include <database.hpp>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
 ThreadManager *thread_manager = NULL;
 class MessageAnalyzer: public IFunct{
     BotApi * bot;
@@ -27,24 +31,53 @@ class MessageAnalyzer: public IFunct{
 
     }
 };
-int main(){
-    BotApi * bot = new BotApi("6458804621:AAHJCmmUNXPQIyQaASR8LFsabxJgVkpC2wA");
-    thread_manager = new ThreadManager;
+int main(int argc, char **argv){
+    
+    //Importing params
+    //loger
+    std::ofstream log("log.txt", std::ios::app);
+    auto t = std::time(0);
+    auto tm = *std::localtime(&t);
+    BotApi * bot;
+    try{
+        SettingsImporter settings("settings.txt");
+        settings.import_settings();
+
+        bot = new BotApi(settings.get_bot_id());
+        thread_manager = new ThreadManager;
+    } catch(SettingsImporterException_NotFound &e){
+        std::cout << "Critical error:";
+         log << "-----------------------------\n"
+        << "BOT FAILED "
+        << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << std::endl
+        << e.what() << std::endl
+        <<"-----------------------------\n";
+    }
+    
+
+    
+
+
+    log << "-----------------------------\n"
+        << "BOT STARTED "
+        << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << std::endl
+        <<"-----------------------------\n";
+        
+    //Getting updates   
     for (;;){
         auto updates = bot->GetUpdates();
         std::cout << "Updates : " << updates.size() << std::endl;
-        
-        for (auto &it : updates){
-            auto msg_anal = new MessageAnalyzer(bot, it);
-            msg_anal->callFunction();
-            thread_manager->add_function(msg_anal);
-            std::cout << "id " << it.update_id << std::endl;
-            std::cout << "from " << it.message.from.first_name << std::endl;
-            std::cout << "text " << it.message.text << std::endl;
-            std::cout << "is document " << std::boolalpha << it.message.is_document << std::endl; 
-            std::cout << "\n\n";
-            
-            
+        if (updates.size()){
+            std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << std::endl;
+            for (auto &it : updates){
+                auto msg_anal = new MessageAnalyzer(bot, it);
+                msg_anal->callFunction();
+                thread_manager->add_function(msg_anal);
+
+                std::cout << "---------Update :" << it.update_id << "-----------------";
+                std::cout << "New message: \nchat_id:" << it.message.chat_id << " message_id: " << it.message.message_id << " user_id:" << it.message.from.user_id << std::endl;
+                log << "New message: \nchat_id:" << it.message.chat_id << " message_id: " << it.message.message_id << " user_id:" << it.message.from.user_id << std::endl;
+            }
         }
     }
 }
